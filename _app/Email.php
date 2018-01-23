943e540cd54551f04f88b2e32317106c06cd67d9
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-23 19:19:47 -0200

    Desenvolvendo Aplicacao: disparo e-mail

--- _app/Email.php
+++ _app/Email.php
@@ -3,17 +3,45 @@
 namespace Notification;
 /* NOME: Vendor, Biblioteca, Método */
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 class Email
 {
+	private $mail = \stdClass::class;
 
-	public function __contruct()
+	public function __construct()
 	{
-		$email = new PHPMailer; 
+		$this->mail = new PHPMailer(true);
+	 	$this->mail->SMTPDebug = 2;                                 // Enable verbose debug output
+	    $this->mail->isSMTP();                                      // Set mailer to use SMTP
+	    $this->mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+	    $this->mail->SMTPAuth = true;                               // Enable SMTP authentication
+	    $this->mail->Username = 'email-remetente@gmail.com';                 // SMTP username
+	    $this->mail->Password = 'senha-email';                           // SMTP password
+	    $this->mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+	    $this->mail->Port = 587; 
+	    $this->mail->Charset = 'utf-8';
+	    $this->mail->setLanguage('br');
+	    $this->mail->isHTML(true);
+	    $this->mail->setFrom('email-destinatario@gmail.com','nome-destinatário');
+
 	}
 
-	public function sendMail()
+	public function sendMail($subject, $body, $replyEmail,$replyName, $addressEmail, $addressName)
 	{
-		echo "Email enviado";
+		$this->mail->Subject = (string)$subject;
+		$this->mail->Body = $body;
+
+		$this->mail->addReplyTo($replyEmail, $replyName);
+		$this->mail->addAddress($addressEmail, $addressName);
+
+		try
+		{
+			$this->mail->send();
+		}
+		catch(Exception $e)
+		{
+			echo "Erro ao enviar o e-mail {$this->mail->ErrorInfo} {$e->getMessage()}";
+		}
 	}	
 }
--- index.php
+++ index.php
@@ -5,4 +5,7 @@ require __DIR__ .'/lib_ext/autoload.php';
 use Notification\Email;
 
 $novoEmail = new Email;
-$novoEmail->sendMail();
+$novoEmail->sendMail("Assunto Teste","<p>Esse é um e-mail de <b>teste</b>!</p>","email-remetentej@gmail.com","Nome Remetente","email.destinatario@gmail.com", "Nome Destinatário");
+
+
+var_dump($novoEmail);
